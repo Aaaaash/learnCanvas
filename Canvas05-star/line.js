6e4902eccf8634f60eb8932c482da0293a6a3a35
sakura-pc <841105127@qq.com>
2016-07-10 12:39:56 +0800

    lineJoin and miterLimit

--- Canvas05-star/line.js
+++ Canvas05-star/line.js
@@ -5,7 +5,16 @@ window.onload=function(){
     var context=canvas.getContext('2d');
     context.lineWidth=10;
     context.strokeStyle="#058";
-    drawStar(context,100,250,400,400,30);
+    // lineJoin        用于指定线条相交时的形态
+    // miter  尖角（默认）
+    // bevel  斜接
+    // round  圆角
+    context.lineJoin="miter";           //斜接
+    // miterLimit指当lineJoin为miter时，所产生的内角与外角之间距离的最大值，默认值为10，一但超过10lineJoin将会自动变为bevel
+    // miterLimit只有当lineJoin为miter才会有用
+    context.miterLimit=20;
+    drawStar(context,20,250,400,400,0);
+    // 当内圆大小变得很小时，lineJoin自动变为bevel
 }
 
 function drawStar(cxt,r,R,x,y,rot){
@@ -17,3 +26,14 @@ function drawStar(cxt,r,R,x,y,rot){
     cxt.closePath();
     cxt.stroke();
 }
+
+
+// 总结  线条属性一共有四种
+
+/*
+    lineWidth:  线条宽度
+    lineCap:    线条两端的形状（默认为butt,round为圆角，square为一个矩形）
+    lineJoin:   线条相交时的形态（默认为miter尖角，bevel为斜接，round为圆角）
+    miterLimit: 当lineJoin为miter时，两条相交的线条所产生的内角与外角之间距离的最大值（较宽的线条），
+                （默认值为10），当最大值超过10，lineJoin会变为bevel
+*/
